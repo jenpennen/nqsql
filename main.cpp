3dce6e55caac11aa2167d877afd9837089fc709c
jenpennen <jennycho35@gmail.com>
2024-10-28 00:35:25 -0700

    include example of query parser without quote parse

--- main.cpp
+++ main.cpp
@@ -2,9 +2,15 @@
 #include <iostream>
 
 int main() {
-    QueryParser parser1("John Doe 'New York' 10027");
+    QueryParser parser1("John Doe Columbia 10027"); // John next Doe next Columbia next 10027 next 
     std::string field = "";
     while (parser1.getNextField(field))
+    {
+        std::cout << field << " next ";
+    }
+    std::cout << std::endl;
+    QueryParser parser2("John Doe 'New York' 10027"); // John next Doe next
+    while (parser2.getNextField(field))
     {
         std::cout << field << " next ";
     }
