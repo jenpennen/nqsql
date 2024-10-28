c21d171ca83ff32ff573f529e72f5e24e3614f60
jenpennen <jennycho35@gmail.com>
2024-10-28 00:52:07 -0700

    add quote parsing functionality

--- main.cpp
+++ main.cpp
@@ -1,17 +1,24 @@
 #include "queryparser.cpp"
 #include <iostream>
 
-int main() {
-    QueryParser parser1("John Doe Columbia 10027"); // John next Doe next Columbia next 10027 next 
+void runTestCase(const std::string& input){
+    QueryParser parser(input);
     std::string field = "";
-    while (parser1.getNextField(field))
-    {
+    while(parser.getNextField(field)){
         std::cout << field << " next ";
     }
     std::cout << std::endl;
-    QueryParser parser2("John Doe 'New York' 10027"); // John next Doe next
-    while (parser2.getNextField(field))
-    {
-        std::cout << field << " next ";
+}
+int main() {
+    std::vector<std::string> testCases = {
+        "John Doe Columbia 10027",
+        "John Doe 'New York' 10027",
+        "Jane Smith 'Los Angeles' 90024"
+    };
+
+    for (const auto& testCase:testCases) {
+        runTestCase(testCase);
     }
+    return 0;
+
 }
--- queryparser.cpp
+++ queryparser.cpp
@@ -27,14 +27,36 @@ public:
         if(m_text[m_start] != '\''){
             size_t end = m_text.find_first_of(" \t\r\n", m_start);
             fieldText = m_text.substr(m_start, end - m_start);
+            // update m_start for the next search, handling end case
             m_start = (end == std::string::npos) ? m_text.size() : end;
             return true; // retrieved a non-quoted field
         }
 
-        return false;
+        // if the field starts with quote, parse quoted field
+        return parseQuotedField(fieldText);
     }
 private:
     std::string m_text;
     size_t m_start;
+
+    bool parseQuotedField(std::string& fieldText) {
+        fieldText.clear(); // clear field output
+
+        while(true){
+            // move past initial quote
+            m_start++;
+            //find next quote quote
+            size_t end = m_text.find('\'', m_start);
+            fieldText += m_text.substr(m_start, end - m_start);
+            m_start = (end != std::string::npos) ? end + 1: m_text.size();
+        
+            //break if we reach the end of the string or next character is not a quote
+            if (m_start == m_text.size() || m_text[m_start] != '\'') {
+                break;
+            }
+            fieldText += '\''; //handle escaped quote by adding it to the field text
+        }
+        return true;
+    }
 };
 //END OF STRING PARSER
